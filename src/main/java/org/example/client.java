b68bbfc7326188f66ca2e45a3bf8514ae76aab69
louisBerlin <louis.froch@gmail.com>
2024-03-26 10:46:39 +0100

    add getter

--- src/main/java/org/example/client.java
+++ src/main/java/org/example/client.java
@@ -1,4 +1,14 @@
 package org.example;
 
 public record client(String name, int age) {
+
+    @Override
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public int age() {
+        return age;
+    }
 }
