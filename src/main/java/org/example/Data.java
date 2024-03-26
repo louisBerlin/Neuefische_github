5e2e0555b2afc2ad5ae2b9cb47577d53e33da42b
louisBerlin <louis.froch@gmail.com>
2024-03-26 12:16:54 +0100

    add equals

--- src/main/java/org/example/Data.java
+++ src/main/java/org/example/Data.java
@@ -1,4 +1,19 @@
 package org.example;
 
+import java.util.Objects;
+
 public record Data(String a, String b, String c) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Data data = (Data) o;
+        return Objects.equals(a, data.a) && Objects.equals(b, data.b) && Objects.equals(c, data.c);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b, c);
+    }
 }
